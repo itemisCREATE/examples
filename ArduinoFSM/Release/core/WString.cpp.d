37100a65a8df05cb2d7e9e4b586712202cf04067
Rene Beckmann <rene.bckmnn@gmail.com>
2016-07-05 16:22:14 +0200

    renamed "ArduinoFSM" to "arduino", and put the gitignore to work - Release folder is excluded.

--- ArduinoFSM/Release/core/WString.cpp.d
+++
@@ -1,5 +0,0 @@
-core/WString.cpp.o: \
- C:/Users/Rene\ Beckmann/yakindu-sct/arduinoPlugin/packages/arduino/hardware/avr/1.6.11/cores/arduino/WString.cpp \
- C:/Users/Rene\ Beckmann/yakindu-sct/arduinoPlugin/packages/arduino/hardware/avr/1.6.11/cores/arduino/WString.h
-
-C:/Users/Rene\ Beckmann/yakindu-sct/arduinoPlugin/packages/arduino/hardware/avr/1.6.11/cores/arduino/WString.h:
--- ArduinoFSM/Release/core/wiring_pulse.S.d
+++
@@ -1,2 +0,0 @@
-core/wiring_pulse.S.o: \
- C:/Users/Rene\ Beckmann/yakindu-sct/arduinoPlugin/packages/arduino/hardware/avr/1.6.11/cores/arduino/wiring_pulse.S

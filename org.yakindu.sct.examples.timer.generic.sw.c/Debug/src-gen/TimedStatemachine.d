103dc2d497c652dbc004bee87e1cf518c4feb856
Robin Herrmann <Robin.Herrmann@itemis.com>
2018-09-26 15:29:06 +0200

    remove timer

--- org.yakindu.sct.examples.timer.generic.sw.c/Debug/src-gen/TimedStatemachine.d
+++
@@ -1,9 +0,0 @@
-src-gen/TimedStatemachine.o: ../src-gen/TimedStatemachine.c \
- ../src-gen/TimedStatemachine.h ../src-gen/../src/sc_types.h \
- ../src-gen/TimedStatemachineRequired.h
-
-../src-gen/TimedStatemachine.h:
-
-../src-gen/../src/sc_types.h:
-
-../src-gen/TimedStatemachineRequired.h:

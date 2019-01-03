b6d4b88ad8125bb0b72949b8a3e43992991537ab
Axel Terfloth <terfloth@itemis.de>
2019-01-03 11:11:33 +0100

    added example for Qt/C++ code generator (labs)

---
+++ org.yakindu.sct.examples.trafficlight.cpp.qt/implementation/main.cpp
@@ -0,0 +1,44 @@
+#include <QObject>
+#include <QQuickItem>
+#include <QQuickView>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+#include "machine/TrafficLightStateMachine.h"
+#include "machine/scqt_timerservice.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+
+
+    TrafficLightStateMachine *machine = new TrafficLightStateMachine(nullptr);
+    ysc::SCTimerService *timerService = new ysc::SCTimerService(machine);
+    machine->setTimer(timerService);
+    machine->init();
+
+    QObject *root = engine.rootObjects().at(0);
+    QObject *control = root->findChild<QObject*>(QString("control"));
+    QObject::connect(control, SIGNAL(pause()), machine, SLOT(standby()));
+    QObject::connect(control, SIGNAL(play()), machine, SLOT(operate()));
+
+    QObject *trafficLight = root->findChild<QObject*>(QString("trafficLight"));
+    QObject::connect(machine, SIGNAL(redOn()), trafficLight, SLOT(redOn()));
+    QObject::connect(machine, SIGNAL(redOff()), trafficLight, SLOT(redOff()));
+    QObject::connect(machine, SIGNAL(yellowOn()), trafficLight, SLOT(yellowOn()));
+    QObject::connect(machine, SIGNAL(yellowOff()), trafficLight, SLOT(yellowOff()));
+    QObject::connect(machine, SIGNAL(greenOn()), trafficLight, SLOT(greenOn()));
+    QObject::connect(machine, SIGNAL(greenOff()), trafficLight, SLOT(greenOff()));
+
+    machine->enter();
+
+    return app.exec();
+}

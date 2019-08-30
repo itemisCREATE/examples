7246f450fd3da242330e2b3509b30acbde3dce6c
dariosb <dariosb@gmail.com>
2019-08-30 10:16:57 -0300

    Setup trace facility on main

--- org.yakindu.sct.examples.rkh.blinky.stm32/src/main.c
+++ org.yakindu.sct.examples.rkh.blinky.stm32/src/main.c
@@ -6,12 +6,14 @@
 /* -------------------------- Development history -------------------------- */
 /*
  *  2016.03.17  LeFr  v1.0.00  Initial version
- *  2019.21.01  LeFr  v1.0.00  Adapted to Yakindu example
+ *  2019.21.08  LeFr  v1.0.00  Adapted to Yakindu example
+ *  2019.22.08  DaBa  v1.0.00  Setup Trace facility
  */
 
 /* -------------------------------- Authors -------------------------------- */
 /*
- *  LeFr  Leandro Francucci  francuccilea@gmail.com
+ *  LeFr  Leandro Francucci lf@vortexmakes.com
+ *  DaBa  Dario Baliña      db@vortexmakes.com
  */
 
 /* --------------------------------- Notes --------------------------------- */
@@ -31,11 +33,32 @@ static RKH_EVT_T *qsto[QSTO_SIZE];
 
 /* ----------------------- Local function prototypes ----------------------- */
 /* ---------------------------- Local functions ---------------------------- */
+static void
+setupTraceFilters(void)
+{
+    RKH_FILTER_ON_GROUP(RKH_TRC_ALL_GROUPS);
+    RKH_FILTER_ON_EVENT(RKH_TRC_ALL_EVENTS);
+    RKH_FILTER_OFF_EVENT(RKH_TE_TMR_TOUT);
+    RKH_FILTER_OFF_EVENT(RKH_TE_SM_STATE);
+    //RKH_FILTER_OFF_EVENT(RKH_TE_SMA_FIFO);
+    //RKH_FILTER_OFF_EVENT(RKH_TE_SMA_LIFO);
+    //RKH_FILTER_OFF_EVENT(RKH_TE_SM_TS_STATE);
+    RKH_FILTER_OFF_EVENT(RKH_TE_SM_DCH);
+    RKH_FILTER_OFF_SMA(blinky);
+    RKH_FILTER_OFF_ALL_SIGNALS();
+}
+
 /* ---------------------------- Global functions --------------------------- */
 int
 main(int argc, char *argv[])
 {
     bsp_init(argc, argv);
+
+    rkh_fwk_init();
+
+    setupTraceFilters();
+
+    RKH_TRC_OPEN();
 
     RKH_SMA_ACTIVATE(blinky, qsto, QSTO_SIZE, 0, 0);
     rkh_fwk_enter();

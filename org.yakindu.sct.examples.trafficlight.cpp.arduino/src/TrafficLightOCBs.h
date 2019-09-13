d1ab99461fa2e63482b302c48e398cf1abb3428c
rherrmannr <Robin.Herrmann@itemis.com>
2019-09-13 14:15:09 +0200

    Arduino cpp example (#149)
    * Traffic light example for Arduino in C++
    
    * removed author
    
    * remove c domain as required dependency
    
    * install libs
    
    * updated image
    
    * updated index.html
    
    * Minor changes

---
+++ org.yakindu.sct.examples.trafficlight.cpp.arduino/src/TrafficLightOCBs.h
@@ -0,0 +1,18 @@
+
+#ifndef TRAFFICLIGHTOCBS_H_
+#define TRAFFICLIGHTOCBS_H_
+
+#include "../src-gen/TrafficLightCtrl.h"
+
+class TrafficLightOCBs : public TrafficLightCtrl::DefaultSCI_OCB {
+public:
+	TrafficLightOCBs(TrafficLightCtrl* machine) : arduino(machine){}
+	virtual ~TrafficLightOCBs();
+
+	void synchronize();
+
+private:
+	TrafficLightCtrl* arduino;
+};
+
+#endif /* TRAFFICLIGHTOCBS_H_ */
---
+++ org.yakindu.sct.examples.trafficlight.cpp.arduino/src/arduinoPins.h
@@ -0,0 +1,19 @@
+#ifndef ARDUINOPINS_H_
+#define ARDUINOPINS_H_
+
+const int button_1_pin = 3;    // the number of the pushbutton 1 pin
+const int button_2_pin = 2;    // the number of the pushbutton 2 pin
+
+const int led_green_pin = 7;
+const int led_yellow_pin = 6;
+const int led_red_pin = 5;
+
+const int ped_led_red_pin = 8;
+const int ped_led_green_pin = 10;
+
+const int ped_led_request_pin = 9;
+
+
+
+
+#endif /* ARDUINOPINS_H_ */

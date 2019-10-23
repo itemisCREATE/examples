9b76261e972d0ec52e1746e41208551782a8c587
Robin Herrmann <Robin.Herrmann@itemis.com>
2019-10-23 16:41:05 +0200

    renaming projects

---
+++ org.yakindu.sct.examples.integration.c.arduino.interrupts/sloeber.ino.cpp
@@ -0,0 +1,16 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2019-10-23 16:39:42
+
+#include "Arduino.h"
+
+
+#error the file: main.ino is not found in the indexer though it exists on the file system.
+#error this is probably due to a bad eclipse configuration : ino and pde are not marked as c++ file.
+#error please check whether *.ino and *.pde are marked as C++ source code in windows->preferences->C/C++->file types.
+
+
+
+#endif

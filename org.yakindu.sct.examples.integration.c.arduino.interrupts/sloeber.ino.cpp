eae7e4fcb133fafa86e3f5199798088e17d00d13
rherrmannr <robinherrmann@hotmail.de>
2020-09-17 12:59:34 +0200

    updated gitignore

--- org.yakindu.sct.examples.integration.c.arduino.interrupts/sloeber.ino.cpp
+++ org.yakindu.sct.examples.integration.c.arduino.interrupts/sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2019-10-24 11:40:16
+//This file has been generated on 2020-09-17 12:57:32
 
 #include "Arduino.h"
 #include "Arduino.h"

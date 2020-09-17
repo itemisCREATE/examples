ed1cdd430a0b5e1237e151bd57674aebc435480b
rherrmannr <robinherrmann@hotmail.de>
2020-09-17 13:55:33 +0200

    sloeber project for zowi

---
+++ org.yakindu.sct.examples.zowi.cpp.arduino/src/ZowiCallbacks.h
@@ -0,0 +1,28 @@
+#include "../src-gen/ZowiSCT.h"
+
+#ifndef SRC_ZOWICALLBACKS_H_
+#define SRC_ZOWICALLBACKS_H_
+
+class ZowiCallbacks : public ZowiSCT::SCI_Zowi_OCB{
+public:
+	ZowiCallbacks();
+	virtual ~ZowiCallbacks();
+
+	void init();
+
+	void home();
+
+	void walk(sc_integer steps, sc_integer T, sc_integer dir);
+
+	void turn();
+
+	sc_real getDistance();
+
+	sc_real getNoise();
+
+	void putMouth(sc_integer mouth);
+
+	void sing(sc_integer songName);
+};
+
+#endif /* SRC_ZOWICALLBACKS_H_ */

2234bbee87da641f4a152b1a65705b94e6252b8e
rherrmannr <robinherrmann@hotmail.de>
2020-10-30 14:22:29 +0100

    updated coffe machine c with new timer service

--- com.yakindu.sct.examples.coffeemachine.c/main.c
+++ com.yakindu.sct.examples.coffeemachine.c/main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 #include <sys/time.h>
 
 #include "src/cm_hmi.h"
 #include "src-gen/CoffeeMachine.h"
-#include "src/sc_timer_service.h"
-
-void setUp();
-void loop();
-int millis(struct timeval *start, struct timeval *stop);
+#include "src-gen/sc_timer_service.h"
 
 /*! \file Implementation of the main function and setup of the virtual YAKINDU Coffee Maker.
  * It includes everything to setup and run the example.
@@ -30,23 +27,22 @@ static sc_timer_t timers[MAX_TIMERS];
 //! The timers are managed by a timer service. */
 static sc_timer_service_t timer_service;
 
-static unsigned long current_millies = 0L;
-static unsigned long last_cycle_time = 0L;
-static struct timeval stop, start;
+unsigned long time_offset = 0;
+unsigned long last_time = 0;
+unsigned long current_time = 0;
+struct timespec sleep_time;
 
-/*! Run the state machine, raise in events and update all timers. */
-void coffeMachine_run(sc_timer_service_t *timer_service,
-		CoffeeMachine *coffee_state_machine) {
-	gettimeofday(&start, NULL);
-	gettimeofday(&stop, NULL);
-	last_cycle_time = millis(&start, &stop);
-	while (1) {
-
-	}
+unsigned long get_ms() {
+	struct timeval tv;
+	unsigned long ms;
+	gettimeofday(&tv, 0);
+	ms = tv.tv_sec * 1000 + (tv.tv_usec / 1000);
+	return ms;
 }
 
 /*! Set up the timer. Init and enter the state machine. */
 void setUp() {
+	time_offset = get_ms();
 	puts("!!!Hello Coffee!!!");
 	puts("general commands are:");
 	puts("(o) toggle on/off");
@@ -56,10 +52,6 @@ void setUp() {
 	// We initialize the timer service with everything it requires.
 	sc_timer_service_init(&timer_service, timers, MAX_TIMERS,
 			(sc_raise_time_event_fp) &coffeeMachine_raise_time_event);
-
-	gettimeofday(&start, NULL);
-	stop = start;
-	last_cycle_time = millis(&start, &stop);
 
 	// then we initialize
 	coffeeMachine_init(&coffee_state_machine);
@@ -71,10 +63,9 @@ void setUp() {
 /*! Loop forever. The run_cycle should be called every 200 ms
  * depending on the @CycleBased() definition. */
 void loop() {
-	current_millies = millis(&start, &stop);
-	if (current_millies >= last_cycle_time + 200) {
-		sc_timer_service_proceed(&timer_service,
-				current_millies - last_cycle_time);
+	current_time = get_ms() - time_offset;
+	if (current_time >= last_time + 200) {
+		sc_timer_service_proceed(&timer_service, current_time - last_time);
 		UserEvents userInput = getUserInput();
 		if (userInput == QUIT) {
 			puts("Bye ...");
@@ -82,17 +73,9 @@ void loop() {
 		}
 		coffeeMachine_raise_userEvent(&coffee_state_machine, userInput);
 		coffeeMachine_run_cycle(&coffee_state_machine);
-		last_cycle_time = current_millies;
+		last_time = current_time;
 	}
-	gettimeofday(&stop, NULL);
-
-}
-
-/*! Get the time difference from start and stop in milliseconds. */
-int millis(struct timeval *start, struct timeval *stop) {
-	return ((stop->tv_sec - start->tv_sec) * 1000
-			+ (stop->tv_usec - start->tv_usec) / 1000
-			+ ((stop->tv_usec - start->tv_usec) % 1000) / 500);
+	nanosleep(&sleep_time, 0);
 }
 
 /*!
@@ -113,7 +96,6 @@ void coffeeMachine_set_timer(CoffeeMachine *handle, const sc_eventid evid,
  \param evid An unique identifier of the event.
  */
 void coffeeMachine_unset_timer(CoffeeMachine *handle, const sc_eventid evid) {
-
 	// simply delegate to the generic timer service implementation.
 	sc_timer_cancel(&timer_service, evid);
 }

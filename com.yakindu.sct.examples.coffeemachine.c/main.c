35ff57048a2ced387182ddc9350435e00b2ff004
rherrmannr <robinherrmann@hotmail.de>
2020-10-30 08:33:02 +0100

    removed pthread timer with sw timer and reformatted all files

--- com.yakindu.sct.examples.coffeemachine.c/main.c
+++ com.yakindu.sct.examples.coffeemachine.c/main.c
@@ -1,35 +1,27 @@
-/*
- * cm_main.c
- *
- *  Created on: 13.04.2016
- *      Author: terfloth
- */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
+#include <sys/time.h>
 
 #include "src/cm_hmi.h"
 #include "src-gen/CoffeeMachine.h"
-#include "src/sc_cycle_runner.h"
 #include "src/sc_timer_service.h"
 
-
+void setUp();
+void loop();
+int millis(struct timeval *start, struct timeval *stop);
 
 /*! \file Implementation of the main function and setup of the virtual YAKINDU Coffee Maker.
- * It includes everything to setup and run the example. The example applies a cycle based execution
- * model and makes use of POSIX threads.
+ * It includes everything to setup and run the example.
+ * The example applies a cycle based execution model.
  */
 
-//! First we create our coffee state machine.
+/* ! First we create our coffee state machine. */
 static CoffeeMachine coffee_state_machine;
 
-//! We additionally make use of a generic cycle runner that invokes the state machines runCycle function at a fixed frequency.
-static sc_cycle_runner_t cycle_runner;
-
-//! As we make use of time triggers (after & every) we make use of a generic timer implementation and need a defined number of timers.
+/* ! As we make use of time triggers (after & every)
+ * we make use of a generic timer implementation
+ * and need a defined number of timers. */
 #define MAX_TIMERS 4
 
 //! We allocate the desired array of timers.
@@ -38,18 +30,23 @@ static sc_timer_t timers[MAX_TIMERS];
 //! The timers are managed by a timer service. */
 static sc_timer_service_t timer_service;
 
-//! For synchronizing the various threads (cycle runner, timers) we create a mutex.
-static pthread_mutex_t coffe_state_machine_mutex = PTHREAD_MUTEX_INITIALIZER;
+static unsigned long current_millies = 0L;
+static unsigned long last_cycle_time = 0L;
+static struct timeval stop, start;
 
+/*! Run the state machine, raise in events and update all timers. */
+void coffeMachine_run(sc_timer_service_t *timer_service,
+		CoffeeMachine *coffee_state_machine) {
+	gettimeofday(&start, NULL);
+	gettimeofday(&stop, NULL);
+	last_cycle_time = millis(&start, &stop);
+	while (1) {
 
-static void run_cycle(void *handle) {
-	coffeeMachine_raise_userEvent(handle, provideUserEvent());
-	coffeeMachine_run_cycle(handle);
+	}
 }
 
-
-int main(void) {
-
+/*! Set up the timer. Init and enter the state machine. */
+void setUp() {
 	puts("!!!Hello Coffee!!!");
 	puts("general commands are:");
 	puts("(o) toggle on/off");
@@ -57,32 +54,46 @@ int main(void) {
 	puts("(q) quit");
 
 	// We initialize the timer service with everything it requires.
-	sc_timer_service_init(
-			&timer_service,
-			timers, MAX_TIMERS,
-			(sc_raise_time_event_fp) &coffeeMachine_raise_time_event,
-			&coffe_state_machine_mutex);
+	sc_timer_service_init(&timer_service, timers, MAX_TIMERS,
+			(sc_raise_time_event_fp) &coffeeMachine_raise_time_event);
+
+	gettimeofday(&start, NULL);
+	stop = start;
+	last_cycle_time = millis(&start, &stop);
 
 	// then we initialize
 	coffeeMachine_init(&coffee_state_machine);
 	// ... and enter the state machine
 	coffeeMachine_enter(&coffee_state_machine);
 
-	// ... start up the cycle runner
-	sc_cycle_runner_start(
-			&cycle_runner,
-			&run_cycle,
-			&coffee_state_machine,
-			100, &coffe_state_machine_mutex);
-
-	// ... and finally process user input.
-	scanUserInput();
-
-	puts("Bye ...");
-
-	return EXIT_SUCCESS;
 }
 
+/*! Loop forever. The run_cycle should be called every 200 ms
+ * depending on the @CycleBased() definition. */
+void loop() {
+	current_millies = millis(&start, &stop);
+	if (current_millies >= last_cycle_time + 200) {
+		sc_timer_service_proceed(&timer_service,
+				current_millies - last_cycle_time);
+		UserEvents userInput = getUserInput();
+		if (userInput == QUIT) {
+			puts("Bye ...");
+			exit(EXIT_SUCCESS);
+		}
+		coffeeMachine_raise_userEvent(&coffee_state_machine, userInput);
+		coffeeMachine_run_cycle(&coffee_state_machine);
+		last_cycle_time = current_millies;
+	}
+	gettimeofday(&stop, NULL);
+
+}
+
+/*! Get the time difference from start and stop in milliseconds. */
+int millis(struct timeval *start, struct timeval *stop) {
+	return ((stop->tv_sec - start->tv_sec) * 1000
+			+ (stop->tv_usec - start->tv_usec) / 1000
+			+ ((stop->tv_usec - start->tv_usec) % 1000) / 500);
+}
 
 /*!
  This function will be called for each time event that is relevant for a state when a state will be entered.
@@ -90,18 +101,26 @@ int main(void) {
  \time_ms The time in milli seconds
  \periodic Indicates the the time event must be raised periodically until the timer is unset
  */
-void coffeeMachine_set_timer(CoffeeMachine* handle, const sc_eventid evid, const sc_integer time_ms, const sc_boolean periodic) {
+void coffeeMachine_set_timer(CoffeeMachine *handle, const sc_eventid evid,
+		const sc_integer time_ms, const sc_boolean periodic) {
 
 	// simply delegate to the generic timer service implementation.
 	sc_timer_start(&timer_service, handle, evid, time_ms, periodic);
 }
 
 /*!
-	This function will be called for each time event that is relevant for a state when a state will be left.
-	\param evid An unique identifier of the event.
-*/
-void coffeeMachine_unset_timer(CoffeeMachine* handle, const sc_eventid evid) {
+ This function will be called for each time event that is relevant for a state when a state will be left.
+ \param evid An unique identifier of the event.
+ */
+void coffeeMachine_unset_timer(CoffeeMachine *handle, const sc_eventid evid) {
 
 	// simply delegate to the generic timer service implementation.
 	sc_timer_cancel(&timer_service, evid);
+}
+
+int main(void) {
+	setUp();
+	for (;;) {
+		loop();
+	}
 }
--- com.yakindu.sct.examples.coffeemachine.c/src/cm_hmi.c
+++ com.yakindu.sct.examples.coffeemachine.c/src/cm_hmi.c
@@ -4,75 +4,100 @@
  *  Created on: 13.04.2016
  *      Author: terfloth
  */
-
-#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <termios.h>
 
 #include "cm_hmi.h"
 
 #include "cm_trace.h"
 
+static char *choice_texts[] = { //
+		"", //
+				"coffee", //
+				"espresso", //
+				"cappucino", //
+				"latte macchiato", //
+				"milk", //
+				"" //
+		};//
 
+/*!
+ * Implementation for non blocking getchar()
+ * https://stackoverflow.com/questions/448944/c-non-blocking-keyboard-input
+ */
+struct termios orig_termios;
 
-static UserEvents lastEvent = NONE;
-
-static char* choice_texts[] = {
-		"",
-		"coffee" ,
-		"espresso" ,
-		"cappucino" ,
-		"latte macchiato" ,
-		"milk" ,
-		""
-};
-
-
-UserEvents provideUserEvent() {
-	UserEvents userEvent = lastEvent;
-	lastEvent = NONE;
-	return userEvent;
+void reset_terminal_mode() {
+	tcsetattr(0, TCSANOW, &orig_termios);
 }
 
+void set_conio_terminal_mode() {
+	struct termios new_termios;
 
-void scanUserInput() {
-	while (1) {
+	/* take two copies - one for now, one for later */
+	tcgetattr(0, &orig_termios);
+	memcpy(&new_termios, &orig_termios, sizeof(new_termios));
+
+	/* register cleanup handler, and set the new terminal mode */
+	atexit(reset_terminal_mode);
+	cfmakeraw(&new_termios);
+	tcsetattr(0, TCSANOW, &new_termios);
+}
+
+int kbhit() {
+	struct timeval tv = { 0L, 0L };
+	fd_set fds;
+	FD_ZERO(&fds);
+	FD_SET(0, &fds);
+	return select(1, &fds, NULL, NULL, &tv);
+}
+
+int getch() {
+	int r;
+	unsigned char c;
+	if ((r = read(0, &c, sizeof(c))) < 0) {
+		return r;
+	} else {
+		return c;
+	}
+}
+
+UserEvents getUserInput() {
+	if (kbhit()) {
 		char c;
 
 		c = getchar();
 
 		switch (c) {
-			case 'o':
-				lastEvent = ON_OFF;
-				break;
-			case '1':
-				lastEvent = COFFEE;
-				break;
-			case '2':
-				lastEvent = ESPRESSO;
-				break;
-			case '3':
-				lastEvent = CAPPUCINO;
-				break;
-			case '4':
-				lastEvent = LATTE_MACCHIATO;
-				break;
-			case '5':
-				lastEvent = MILK;
-				break;
-			case 't':
-				cm_trace_active = !cm_trace_active;
-				break;
-			case 'q':
-				return;
-			case '\n':
-				break;
-			default:
-				lastEvent = NONE;
+		case 'o':
+			return ON_OFF;
+		case '1':
+			return COFFEE;
+		case '2':
+			return ESPRESSO;
+		case '3':
+			return CAPPUCINO;
+		case '4':
+			return LATTE_MACCHIATO;
+		case '5':
+			return MILK;
+		case 't':
+			cm_trace_active = !cm_trace_active;
+			return NONE;
+		case 'q':
+			return QUIT;
+		default:
+			return NONE;
 		}
 	}
+	return NONE;
 }
 
-void show(char* text) {
+void show(char *text) {
 
 	fprintf(stdout, "%s", text);
 	fflush(stdout);
@@ -89,6 +114,4 @@ void showDone(UserEvents choice) {
 	fprintf(stdout, "Take and enjoy your %s\n", choice_texts[choice]);
 	fflush(stdout);
 }
-
-
 

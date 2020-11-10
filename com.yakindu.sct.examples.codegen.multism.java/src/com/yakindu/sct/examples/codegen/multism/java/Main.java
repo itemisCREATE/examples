3944f44d98407c8f2d4cdc38ea36ce08049149f8
Thomas Kutz <thomas.kutz@itemis.de>
2020-11-10 14:37:02 +0100

    Add time event to light statechart

--- com.yakindu.sct.examples.codegen.multism.java/src/com/yakindu/sct/examples/codegen/multism/java/Main.java
+++ com.yakindu.sct.examples.codegen.multism.java/src/com/yakindu/sct/examples/codegen/multism/java/Main.java
@@ -9,15 +9,17 @@ import com.yakindu.sct.examples.codegen.multism.java.light.Light;
 public class Main {
 
 	public static void main(String[] args) {
-		/* Instantiates the controller state machine */
+		/* Instantiates the state machines */
 		LightController controller = new LightController();
+		Light light1 = new Light();
+		Light light2 = new Light();
 		
 		/* Sets the timer service */
 		controller.setTimerService(new TimerService());
+		light1.setTimerService(new TimerService());
+		light2.setTimerService(new TimerService());
 
 		/* Instantiates and sets the sub machines */
-		Light light1 = new Light();
-		Light light2 = new Light();
 		controller.setLight1(light1);
 		controller.setLight2(light2);
 
@@ -40,13 +42,13 @@ public class Main {
 		while (!controller.isFinal()) {
 			String action = sc.next();
 			if ("On".equals(action)) {
-				/* Raises the On event in the state machine which causes the corresponding transition to be taken */
+				/* Raises the switch_on event in the state machine which causes the corresponding transition to be taken */
 				controller.user().raiseSwitch_on();
 			} else if ("Off".equals(action)) {
-				/* Raises the Off event in the state machine */
+				/* Raises the switch_off event in the state machine */
 				controller.user().raiseSwitch_off();
 			} else if ("Blink".equals(action)) {
-				/* Raises the Off event in the state machine */
+				/* Raises the blink event in the state machine */
 				controller.user().raiseBlink_mode();
 			}
 		}

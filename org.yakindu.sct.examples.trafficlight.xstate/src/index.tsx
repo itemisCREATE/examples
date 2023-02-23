aeb52b31959ffd635f02cb4a054c776be6e94879
Mazesch <mschulte@itemis.com>
2023-02-23 21:16:57 +0100

    fixed example

--- org.yakindu.sct.examples.trafficlight.xstate/src/index.tsx
+++ org.yakindu.sct.examples.trafficlight.xstate/src/index.tsx
@@ -1,39 +1,85 @@
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-import { createMachine, assign } from "xstate";
-import { useMachine } from "@xstate/react";
-import {TrafficLightCtrl, StatechartContext} from "./TrafficLightCtrl";
-import {SceneApp, setTrafficLights, setPedestrianLights} from "./scene";
+import { interpret } from "xstate";
+import {TrafficLightCtrl} from "./TrafficLightCtrl";
 import { inspect } from '@xstate/inspect';
+import "./style.css"
 
+inspect({
+  iframe: () => document.querySelector("iframe[data-xstate]") as HTMLIFrameElement
+});
 
-
-
-
-function App() {
-  const [current, send] = useMachine(TrafficLightCtrl, {context:new StatechartContext(), devTools: true});
-  current.context.TrafficLight.off$.subscribe(() => setTrafficLights(false, false, false))
-  current.context.TrafficLight.red$.subscribe(() => setTrafficLights(true, false, false))
-  current.context.TrafficLight.preparing$.subscribe(() => setTrafficLights(true, true, false))
-  current.context.TrafficLight.yellow$.subscribe(() => setTrafficLights(false, true, false))
-  current.context.TrafficLight.green$.subscribe(() => setTrafficLights(false, false, true))
-
-  
-  current.context.Pedestrian.off$.subscribe(() => setPedestrianLights(false, false))
-  current.context.Pedestrian.red$.subscribe(() => setPedestrianLights(true, false))
-  current.context.Pedestrian.green$.subscribe(() => setPedestrianLights(false, true))
-
-  return SceneApp(() => send("onOff"), () => send("pedestrianRequest"));
+function setTrafficLights(red : boolean, yellow : boolean, green : boolean){
+  if(red){
+    document.getElementById("redLight").style.visibility = 'visible';
+  }else{
+    document.getElementById("redLight").style.visibility = 'hidden';
+  }
+  if(yellow){
+    document.getElementById("yellowLight").style.visibility = 'visible';
+  }else{
+    document.getElementById("yellowLight").style.visibility = 'hidden';
+  }
+  if(green){
+    document.getElementById("greenLight").style.visibility = 'visible';
+  }else{
+    document.getElementById("greenLight").style.visibility = 'hidden';
+  }
 }
 
+function setPedestrianLights(pedestrian1 : boolean, pedestrian2 : boolean){  
+  if(pedestrian1){
+    document.getElementById("pedestrianLight1").style.visibility = 'visible';
+  }else{
+    document.getElementById("pedestrianLight1").style.visibility = 'hidden';
+  }
+  if(pedestrian1){
+    document.getElementById("pedestrianLight2").style.visibility = 'visible';
+  }else{
+    document.getElementById("pedestrianLight2").style.visibility = 'hidden';
+  }
+}
 
+document.getElementById("app").innerHTML = `
+<div class="divClass">
+    <div class="flexRowHere">
+        <div class="scene">
+            <img src="./images/road.png" id="roadImage"></img>
+            <img src="./images/trafficLightAllOff.png" id="trafficLight"></img>
+            <img src="./images/red.png" id="redLight" class="lamp"></img>
+            <img src="./images/yellow.png" id="yellowLight" class="lamp"></img>
+            <img src="./images/green.png" id="greenLight" class="lamp"></img>
+            <img src="./images/yellow.png" class="yellowPedestrianLight pedestrianLight1 lamp" id="pedestrianLight1"></img>
+            <img src="./images/yellow.png" class="yellowPedestrianLight pedestrianLight2 lamp" id="pedestrianLight2"></img>
+            <img src="./images/French-Pedestrian-Silhouette.svg" id="pedestrianImage"></img>
+            <div class="buttons">
+                <button id="button1" class="button1" type="button" onClick="functionOnOff()">ON/OFF</button>
+                <button id="button2" class="button2" type="button" onClick="functionRequest()">Request</button>
+            </div>
+        </div>
+    </div>
+</div>
+`;
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-const d = root.render(<App />);
+document.getElementById("button1").onclick = function(){
+  service.send("onOff")
+}
+document.getElementById("button2").onclick = function(){
+  service.send("pedestrianRequest")
+}
 
-// how to find that part of document, which might not be rendered yet
-inspect({
-  iframe: false,//() => (document.querySelector('iframe data-xstate') as HTMLIFrameElement),
-  url: 'https://statecharts.io/inspect'
-});
+const service = interpret(TrafficLightCtrl, { devTools: true }).onTransition(
+  (state) => {
+    console.log(state.value);
+  }
+);
+
+service.machine.context.TrafficLight.off$.subscribe(() => setTrafficLights(false, false, false))
+service.machine.context.TrafficLight.red$.subscribe(() => setTrafficLights(true, false, false))
+service.machine.context.TrafficLight.preparing$.subscribe(() => setTrafficLights(true, true, false))
+service.machine.context.TrafficLight.yellow$.subscribe(() => setTrafficLights(false, true, false))
+service.machine.context.TrafficLight.green$.subscribe(() => setTrafficLights(false, false, true))
+service.machine.context.Pedestrian.off$.subscribe(() => setPedestrianLights(false, false))
+service.machine.context.Pedestrian.red$.subscribe(() => setPedestrianLights(true, false))
+service.machine.context.Pedestrian.green$.subscribe(() => setPedestrianLights(false, true))
+
+service.start();
+
